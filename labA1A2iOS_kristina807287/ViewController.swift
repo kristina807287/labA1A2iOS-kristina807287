44faaa3be27e6aa0d95a7e4d2c7bc4c9716cd7aa
kristina807287 <c0807287@mylambton.ca>
2021-09-19 22:09:57 -0400

    Initial Commit

---
+++ labA1A2iOS_kristina807287/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  labA1A2iOS_kristina807287
+//
+//  Created by user203929 on 9/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
